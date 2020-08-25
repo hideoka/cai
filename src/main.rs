2eab727a708c33bfe9078d9c663be0a773754601
Hideaki Nishioka <hideaki.nishioka@gmail.com>
2020-08-25 22:52:59 +0900

    Implement command interface

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,17 @@ use std::process::Command;
 #[macro_use]
 extern crate clap;
 
+struct Cmd<'a> {
+    command: &'a str,
+    args: Option<Vec<&'a str>>,
+}
+
+impl<'a> Cmd<'a> {
+    fn new(command: &'a str, args: Option<Vec<&'a str>>) -> Cmd<'a> {
+        Cmd { command, args }
+    }
+}
+
 fn run() -> Result<(), Box<dyn Error>> {
     let args = env::args_os();
 
@@ -14,15 +25,22 @@ fn run() -> Result<(), Box<dyn Error>> {
         .version(crate_version!())
         .author(crate_authors!())
         .arg(Arg::with_name("command").index(1).required(true))
+        .arg(Arg::with_name("args").min_values(0))
         .get_matches_from(args);
 
-    match matches.value_of("command") {
-        Some(command) => {
-            Command::new("bash").arg("-c").arg(command).spawn()?;
-            Ok(())
-        }
-        None => unimplemented!(),
+    let command = matches.value_of("command").unwrap();
+    let args = matches.values_of("args").map(|a| a.collect::<Vec<_>>());
+    let cmd = Cmd::new(command, args);
+
+    if let Some(args) = cmd.args {
+        Command::new("bash")
+            .arg("-c")
+            .arg(format!("{:?} {:?}", cmd.command, args.join(" ")))
+            .spawn()?;
+    } else {
+        Command::new("bash").arg("-c").arg(cmd.command).spawn()?;
     }
+    Ok(())
 }
 
 fn main() {
